55e33c458bef570541c5e09c1cbc979203fa1aab
Alezzo99 <alejandrommp4.oficial@gmail.com>
2023-09-13 14:48:25 -0400

    Terminados tests

--- src/test/java/com/idos/apk/backend/tienda/tatuajes/service/DetalleOrdenServiceImpTest.java
+++ src/test/java/com/idos/apk/backend/tienda/tatuajes/service/DetalleOrdenServiceImpTest.java
@@ -2,6 +2,7 @@ package com.idos.apk.backend.tienda.tatuajes.service;
 
 import com.idos.apk.backend.tienda.tatuajes.dto.detalle.DetalleOrdenInDto;
 import com.idos.apk.backend.tienda.tatuajes.dto.detalle.DetalleOrdenOutDto;
+import com.idos.apk.backend.tienda.tatuajes.exceptions.ProductoNotFoundException;
 import com.idos.apk.backend.tienda.tatuajes.mapper.DetalleMapper;
 import com.idos.apk.backend.tienda.tatuajes.model.*;
 import com.idos.apk.backend.tienda.tatuajes.repository.BlackListRepo;
@@ -11,6 +12,7 @@ import com.idos.apk.backend.tienda.tatuajes.repository.ProductoRepository;
 import com.idos.apk.backend.tienda.tatuajes.service.interfaces.DetalleOrdenService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,8 +21,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
 @Slf4j
 class DetalleOrdenServiceImpTest {
@@ -34,13 +40,15 @@ class DetalleOrdenServiceImpTest {
     DetalleMapper mapper;
 
     @InjectMocks
-    DetalleOrdenService service;
+    DetalleOrdenServiceImp service;
 
     DetalleOrdenInDto detalleOrdenInDto;
 
     DetalleOrdenOutDto detalleOrdenOutDto;
 
     DetalleOrden detalleOrden;
+    Orden orden;
+    Producto producto;
 
     @BeforeEach
     void setUp() {
@@ -55,8 +63,9 @@ class DetalleOrdenServiceImpTest {
         TipoProducto tipoProducto = TipoProducto.builder()
                 .name("Tipo").build();
 
-        Producto producto = Producto.builder()
-                .cantidad(2)
+        producto = Producto.builder()
+                .id("Producto")
+                .cantidad(2222)
                 .descripcion("des")
                 .tipo(tipoProducto)
                 .img("Imagen")
@@ -64,22 +73,55 @@ class DetalleOrdenServiceImpTest {
                 .nombre("Producto")
                 .build();
 
-       Orden orden = Orden.builder()
-                .detalle(List.of(detalleOrden))
+       orden = Orden.builder()
+               .id("orden")
                 .total(2.2)
                 .usuario(usuario).build();
-        detalleOrdenInDto = DetalleOrdenInDto.builder()
+       detalleOrdenInDto = DetalleOrdenInDto.builder()
                 .cantidad(22)
                 .id_producto("Producto")
                 .total(2.2)
                 .build();
 
-        detalleOrden = DetalleOrden.builder()
+       detalleOrden = DetalleOrden.builder()
                 .id("idDetalle")
                 .cantidad(22)
                 .total(2.2)
                 .orden(orden)
                 .producto(producto)
                 .build();
+    }
+
+
+    @Test
+    void testSaveMethod(){
+        when(productoRepository.findById("Producto")).thenReturn(Optional.of(producto));
+        when(productoRepository.getReferenceById("Producto")).thenReturn(producto);
+        when(ordenRepository.getReferenceById("orden")).thenReturn(orden);
+        when(mapper.detalleInToDetalle(detalleOrdenInDto)).thenReturn(detalleOrden);
+
+        DetalleOrdenInDto b = service.save(detalleOrdenInDto, "orden");
+
+        // Assert
+        verify(repository, times(1)).save(any());
+        assertAll(
+                ()-> assertNotNull(b),
+                ()-> assertEquals(detalleOrdenInDto.getTotal(), b.getTotal()),
+                ()-> assertEquals(detalleOrdenInDto.getId_producto(), b.getId_producto())
+        );
+        log.info("Save Test Pass");
+    }
+    @Test
+    void testSaveMethodProductoNotFoundException(){
+        doThrow(new ProductoNotFoundException("Produto no encontrado")).when(productoRepository).findById("Producto");
+
+
+        // Assert
+
+        assertAll(
+                ()-> assertThrows(ProductoNotFoundException.class, () -> service.save(detalleOrdenInDto, "orden" ))
+        );
+
+        log.info("SaveProductNotFoundException Test Pass");
     }
 }
